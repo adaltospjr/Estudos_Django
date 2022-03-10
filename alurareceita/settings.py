f1f663d0b84eb42bd52a78dd419a4136004ef154
Adalto Linhares <adaltospjr@gmail.com>
2022-03-10 15:03:36 -0300

    Adicionando imagens para as receitas.

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -126,3 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'alurareceita/static')
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL= '/media/'
